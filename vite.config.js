03c4fc7ecd97fa1cd901e4d292139f2cf1ca91b4
mifta-kedir <49817419+mifta-kedir@users.noreply.github.com>
2025-12-10 20:22:01 -0500

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🔑 FIX: Add the base property set to your GitHub repository name
+  base: "/Netflix-Clone-2025/",
 });
