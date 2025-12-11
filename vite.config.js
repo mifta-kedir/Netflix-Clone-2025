f5439466f97746a55d42fe30930e14ea72ba309d
mifta-kedir <49817419+mifta-kedir@users.noreply.github.com>
2025-12-10 19:59:47 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
+// https://vite.dev/config/
 export default defineConfig({
-  // ...other settings
-  base: "/Netflix-Clone-2025/", // <-- This is the key fix
-  // ...other settings
+  plugins: [react()],
 });
